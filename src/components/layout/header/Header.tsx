11320a90ca0f88e012aed8b60631811119e57f25
Noé Gambini <noe.gambini@gmail.com>
2018-05-23 21:26:01 +0200

    [+] Common theming

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 
-import {  Navbar, NavbarGroup, NavbarHeading, NavbarDivider, Alignment, Button,  } from '@blueprintjs/core'
+import { Navbar, NavbarGroup, NavbarHeading, NavbarDivider, Alignment, Button } from '@blueprintjs/core'
+
+import mainLogo from '../../../assets/images/pubg-transparent-logo.png'
 
 export default class Header extends React.Component {
   public render() {
@@ -8,7 +10,11 @@ export default class Header extends React.Component {
       <header>
         <Navbar>
           <NavbarGroup align={Alignment.LEFT}>
-            <NavbarHeading>Blueprint</NavbarHeading>
+            <NavbarHeading>PUBG Game Reports</NavbarHeading>
+            <NavbarDivider />
+            <NavbarHeading>
+              <img src={mainLogo} />
+            </NavbarHeading>
             <NavbarDivider />
             <Button className="pt-minimal" icon="home" text="Home" />
             <Button className="pt-minimal" icon="search" text="Choose game" />
